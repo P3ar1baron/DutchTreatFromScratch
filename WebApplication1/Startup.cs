b48b34b3381222f4d547baf8531dc567606ae8a1
P3ar1baron <v.rozanoff@gmail.com>
2019-03-17 19:47:00 +0200

    added jQuery functions to work with form, animation after click on Login button

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -22,6 +22,7 @@ namespace WebApplication1
         {
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseNodeModules(env);
         }
     }
 }
--- WebApplication1/wwwroot/js/index.js
+++ WebApplication1/wwwroot/js/index.js
@@ -1,16 +1,29 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
 
-console.log("Hello Pluralshight");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var x = 0;
+    var s = "";
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
+    console.log("Hello Pluralshight");
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    })
 });
-
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
-
