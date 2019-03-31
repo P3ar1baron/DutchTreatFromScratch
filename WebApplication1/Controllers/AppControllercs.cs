5a65b628d5bd8f7c15fd0a07063e9028882f82b5
P3ar1baron <v.rozanoff@gmail.com>
2019-03-31 11:57:15 +0300

    Added Shop view based on Product model

--- WebApplication1/Controllers/AppControllercs.cs
+++ WebApplication1/Controllers/AppControllercs.cs
@@ -1,4 +1,5 @@
-﻿using DutchTreat.Services;
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +12,14 @@ namespace DutchTreat.Controllers
     public class AppController : Controller
     {
         private readonly IMailService _mailService;
+        private readonly DutchContext _context;
 
-        public AppController(IMailService mailService)
+        public DutchContext Context { get; }
+
+        public AppController(IMailService mailService, DutchContext context)
         {
             _mailService = mailService;
+            _context = context;
         }
         public IActionResult Index()
         {
@@ -51,6 +56,15 @@ namespace DutchTreat.Controllers
             ViewBag.Title = "About Us";
 
             return View();
+        }
+
+        public IActionResult Shop()
+        {
+            var results = from p in _context.Products
+                          orderby p.Category
+                          select p;
+                
+            return View(results.ToList());
         }
     }
 }
