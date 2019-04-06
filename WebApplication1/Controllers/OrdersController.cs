2f209433a8922916ba06973d8c594b9cc791e672
P3ar1baron <v.rozanoff@gmail.com>
2019-04-06 23:12:32 +0300

    Start of IActionResult Get method implementation in OrderController

--- WebApplication1/Controllers/OrdersController.cs
+++ WebApplication1/Controllers/OrdersController.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DutchTreat.Controllers
 {
-    public class OrdersController
+    [Route("api/[Controller]")]
+    public class OrdersController : Controller
     {
+        private readonly IDutchRepository _repository;
+        private ILogger<OrdersController> _logger;
+
+        public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+         [HttpGet]
+         public IActionResult Get()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+            
     }
-}
+ }
+
